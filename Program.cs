2292a3e2a947eac3783346d26c5ae36fb6742e69
Danny Le <le.dannyj@gmail.com>
2021-04-22 14:57:46 -0400

    Added Welcome

--- Program.cs
+++ Program.cs
@@ -2,11 +2,20 @@
 
 namespace FirstBankOfSuncoast
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+
+            //First we’re creating a greeting/ welcome for “SDG Bank Monies Manager” 
+
+            Console.WriteLine();
+            Console.WriteLine(new String('*', 24));
+            Console.WriteLine("SDG Bank Monies Manager!");
+            Console.WriteLine(new String('*', 24));
+            Console.WriteLine();
+
+
+        }
     }
-  }
 }
