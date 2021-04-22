d577d0ad82f93342f7ec9e821d833d939faafe6f
Danny Le <le.dannyj@gmail.com>
2021-04-22 15:27:28 -0400

    added prompt inputs and created a class database

--- Program.cs
+++ Program.cs
@@ -2,8 +2,48 @@
 
 namespace FirstBankOfSuncoast
 {
+
+    class Database
+    {
+
+
+        public int TotalAmount { get; set; }
+
+        public string Saving { get; set; }
+        public string Checking { get; set; }
+
+    }
+
     class Program
     {
+        // code to take user's string prompt input
+        static string PromptForString(string prompt)
+        {
+
+
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
+            return userInput;
+        }
+
+        // code to take user's int prompt input
+        // got this code form the example to make sure inputting something random won't crash program.
+        static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+            int userInput;
+            var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+            if (isThisGoodInput)
+            {
+                return userInput;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that isn't a valid input, I'm using 0 as your answer.");
+                return 0;
+            }
+        }
+
         static void Main(string[] args)
         {
 
